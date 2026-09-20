ada6aa2816d27b7a873a6ca5bc6a2ba9465f0c07
Rahulmallella <60692488+Rahulmallella@users.noreply.github.com>
2026-09-20 16:42:57 -0400

    Use Expo Router hook for Filters navigation

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,5 +1,5 @@
 import { Ionicons } from "@expo/vector-icons";
-import { Link, router } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
